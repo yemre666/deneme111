126e875c9d0621e28085d5750a75eb5da4ba24f9
Yunus Emre Aytemur <>
2021-11-06 21:18:18 +0300

    deneme ilk commit

---
+++ GitProjectTest1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitProjectTest1
+//
+//  Created by Yunus Emre Aytemur on 6.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var ali: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        print("git")
+        print("deneme")
+    }
+
+    func xy () {
+        print("perde düştü")
+        print("elifle perde taktık")
+    }
+}
+
+
